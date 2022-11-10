e5ae3a02ec276b59149f07e8b0a2a620fe7a52bc
Roman Furman <roman.furman@uptech.team>
2022-11-10 15:03:16 +0200

    start api

--- launch.py
+++ launch.py
@@ -238,15 +238,5 @@ def tests(argv):
     proc.kill()
 
 
-def start():
-    print(f"Launching {'API server' if '--nowebui' in sys.argv else 'Web UI'} with arguments: {' '.join(sys.argv[1:])}")
-    import webui
-    if '--nowebui' in sys.argv:
-        webui.api_only()
-    else:
-        webui.webui()
-
-
 if __name__ == "__main__":
     prepare_enviroment()
-    start()
---
+++ start_api.py
@@ -0,0 +1,9 @@
+import sys
+
+def start():
+    import webui
+    webui.api_only()
+
+
+if __name__ == "__main__":
+    start()
